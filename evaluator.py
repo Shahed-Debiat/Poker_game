b40a7d6685fa509035f0a2c41a287431da7e99fd
PwNT5Un3 <max@welsch.io>
2026-05-28 09:27:53 +0200

    started work on the evaluator

--- evaluator.py
+++ evaluator.py
@@ -11,15 +11,25 @@ from cards import Card
 
 
 class HandCategory(IntEnum):
-    # TODO: Task 1 - define the hand categories in order from lowest to highest
-    pass
+    HIGH_CARD = 0
+    ONE_PAIR = 1
+    TWO_PAIR = 2
+    THREE_OF_A_KIND = 3
+    STRAIGHT = 4
+    FLUSH = 5
+    FULL_HOUSE = 6
+    FOUR_OF_A_KIND = 7
+    STRAIGHT_FLUSH = 8
 
 
 @dataclass(frozen=True, order=True)
 class HandRank:
-    # TODO: Task 2 - define the HandRank dataclass with the category and tiebreakers
+    category: HandCategory
+    _label: str
 
-    # TODO: Task 2 - return a version of the category
+    @property
+    def label(self) -> str:
+        return self.category.name.replace('_', '').title()
     pass
 
 
@@ -27,8 +37,9 @@ class HandEvaluator:
     # TODO: Task 3 - implement the best_rank method to evaluate the best possible hand from a list of cards
     def best_rank(self, cards: list[Card]) -> HandRank:
         if len(cards) < 5:
-            pass 
-        pass 
+            ValueError("At least five cards required!")
+        match len(cards):
+            case _: raise ValueError("At most seven cards expected!")
 
     def _rank_five(self, cards: list[Card]) -> HandRank:
         # TODO: Task 4 - implement the logic to rank a five-card hand according to poker rules
