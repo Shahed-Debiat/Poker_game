acdb802109378c794e5d09aa9f67647a33ce047d
Akram Guediri <akramguediri2@gmail.com>
2026-05-28 11:30:17 +0200

    small fix in player

--- player.py
+++ player.py
@@ -11,11 +11,11 @@ from cards import Card
 class Player:
     name: str
     # TODO: Task 1 - add chips, is_human, hole_cards, current_bet, and folded
-    chips = 0
-    is_human = False
+    chips: int = 0
+    is_human: bool = False
     hole_cards: list[Card] = field(default_factory=list)
-    current_bet = 0
-    folded = False
+    current_bet: int = 0
+    folded: bool = False
 
     def reset_for_hand(self) -> None:
         # TODO: Task 2 - reset the player's state for a new hand
